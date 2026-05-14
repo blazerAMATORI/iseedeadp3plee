5a0f7cad08a54d958df123408d63dd2334d3dffb
blazerAMATORI <83265801+blazerAMATORI@users.noreply.github.com>
2026-05-14 05:23:02 +0500

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "@/components/ThemeProvider";
-import { Toaster } from "@/components/ui/sonner";
-import GlobalClientEffects from "@/components/GlobalClientEffects";
-
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Dota 2 Quiz - Онлайн Квиз",
@@ -26,27 +11,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const content = (
-    <>
-      {children}
-      <GlobalClientEffects />
-    </>
-  );
-
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
-      >
-        <Toaster />
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {content}
-        </ThemeProvider>
+    <html lang="ru">
+      <body className="antialiased">
+        {children}
       </body>
     </html>
   );
